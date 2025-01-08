247ee4ee146dbdb7ccf2f186bfdafa4da53e9129
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-01-08 22:29:40 +0000

    pyramid recursive solution

--- varDataAlgorithms/OwnCasts/pyramid/pyramid.js
+++ varDataAlgorithms/OwnCasts/pyramid/pyramid.js
@@ -7,6 +7,40 @@ each level of the pyramid that isn't the *last* should have spaces on both the l
 
 */
 
+const pyramid = (n, row = 0, level = "") => {
+
+    const midpoint = Math.floor((2 * n - 1) / 2);
+
+    if (row === n) return;
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+
+        return pyramid(n, row + 1);
+    }
+
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        // if both conditions are true, we are in a column that should have "#"
+        // once we're past the first row (so row !== 0), it also makes columns on either side of the midpoint = "#"
+        // on the third (and final row in this case), it makes all the columns "#"
+        level += "#";
+    } else {
+        // otherwise we are not, and it should have " "
+        level += " ";
+    }
+
+    pyramid(n, row, level);
+
+};
+
+pyramid(1);
+
+module.exports = pyramid;
+
+/*
+
+ITERATIVE SOLUTION
+
 const pyramid = (n) => {
 
     // identify midpoint in row
@@ -21,6 +55,8 @@ const pyramid = (n) => {
 
             if (midpoint - row <= col && midpoint + row >= col) {
                 // if both conditions are true, we are in a column that should have "#"
+                // once we're past the first row (so row !== 0), it also makes columns on either side of the midpoint = "#"
+                // on the third (and final row in this case), it makes all the columns "#"
                 level += "#";
             } else {
                 // otherwise we are not, and it should have " "
@@ -32,49 +68,5 @@ const pyramid = (n) => {
     }
 };
 
-pyramid(3);
-
-module.exports = pyramid;
-
-/*
-
-// prints an equal amount of columns for each row
-
-   for (let row = 0; row < n; row++) {
-
-        let level = "";
-
-        console.log("row = " + row);
-
-        for (let col = 0; col <= n + 2; col++) {
-
-            console.log("col = " + col);
-        }
-
-    }
-
-    PRINTS
-
-row = 0
-col = 0
-col = 1
-col = 2
-col = 3
-col = 4
-col = 5
-row = 1
-col = 0
-col = 1
-col = 2
-col = 3
-col = 4
-col = 5
-row = 2
-col = 0
-col = 1
-col = 2
-col = 3
-col = 4
-col = 5
 
 */
