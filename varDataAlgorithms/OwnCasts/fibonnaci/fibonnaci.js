c4c02c7365ef95d66ff7e7b920e540185aaeb392
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-01-14 12:05:44 +0000

    memoized fib solution

--- varDataAlgorithms/OwnCasts/fibonnaci/fibonnaci.js
+++ varDataAlgorithms/OwnCasts/fibonnaci/fibonnaci.js
@@ -8,19 +8,46 @@
 // Example:
 //   fib(4) === 3
 
-const fib = (n) => {
+
+const memoize = (fn) => {
+
+    const cache = {};
+
+    // represents the fn we pass in to memoize()
+    return function (...args) {
+
+        // `...args` to allow for unknown no. of arguments
+
+        // checks if cache obj already contains any of the args
+        // if so, return the value of the arg
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        // making it past the `if` means we've never called the fn with that arg before, that is - that value hasn't been computed
+
+        const result = fn.apply(this, args); // whenever you call a function with an arr of arguments (i.e. args) we have to use the `apply()` helper
+
+        // store the result of the above computation into our cache and return it
+        // the next time the fn is called with the same argument as previously computed it will return within our `if` check
+        cache[args] = result;
+
+        return result;
+    };
+};
+
+const slowFib = (n) => {
 
     // base case
 
     if (n < 2) return n;
 
-    // keep calling fib(n - 1) + fib (n - 2), until their sum < 2
-    // keep adding the result of those calls where sum < 2 until
-    // it equals a discrete number that is nth in the fibonnaci sequence
-    return fib(n - 1) + fib(n - 2);
+    return fib(n - 1) + fib(n - 2); // recursively call the memoized version of the fib function
+
 };
 
-console.log(fib(20));
+// call our memoize function, passing our recursive fib function as the arg
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
@@ -39,7 +66,73 @@ const fib = (n) => {
 
 */
 
-/* fib #1 - recursive
+/* fib #1 - recursive, O(2 ^ n) exponential time
 
+    for every increase in N we get a dramatic increase in the amount of function calls we make
+
+    const fib = (n) => {
+
+        // base case
+
+        if (n < 2) return n;
+
+        // keep calling fib(n - 1) + fib (n - 2), until their sum < 2
+        // keep adding the result of those calls where sum < 2 until
+        // it equals a discrete number that is nth in the fibonnaci sequence
+        return fib(n - 1) + fib(n - 2);
+    };
+
+*/
+
+/* fib #2 - memoized recursion
+
+
+        improvement on recursive solution - *memoization*
+
+        store the argumnets of each function call along with the result
+        if the function is called again with the same arguments, return the precomputed result instead of running the function again
+
+        dramatically reduces the runtime complexity of the recursive solution
+
+
+        const memoize = (fn) => {
+
+    const cache = {};
+
+    // represents the fn we pass in to memoize()
+    return function (...args) {
+
+        // `...args` to allow for unknown no. of arguments
+
+        // checks if cache obj already contains any of the args
+        // if so, return the value of the arg
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        // making it past the `if` means we've never called the fn with that arg before, that is - that value hasn't been computed
+
+        const result = fn.apply(this, args); // whenever you call a function with an arr of arguments (i.e. args) we have to use the `apply()` helper
+
+        // store the result of the above computation into our cache and return it
+        // the next time the fn is called with the same argument as previously computed it will return within our `if` check
+        cache[args] = result;
+
+        return result;
+    };
+};
+
+const slowFib = (n) => {
+
+    // base case
+
+    if (n < 2) return n;
+
+    return fib(n - 1) + fib(n - 2); // recursively call the memoized version of the fib function
+
+};
+
+// call our memoize function, passing our recursive fib function as the arg
+const fib = memoize(slowFib);
 
 */
