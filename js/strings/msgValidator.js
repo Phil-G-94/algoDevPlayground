1e554da9561143f301c896de8884ecf688a9d8bf
Philippos Georgiou <philipposgeorgiou7@gmail.com>
2025-06-30 22:12:15 +0100

    add msgValidator solution

---
+++ js/strings/msgValidator.js
@@ -0,0 +1,19 @@
+const msgValidator = (inputStr) => {
+  const regexp = /(\d+)([a-zA-Z]+)/g;
+
+  if (inputStr === "") return true;
+
+  let match;
+  let totalLength = 0;
+
+  while ((match = regexp.exec(inputStr)) !== null) {
+    const num = Number(match[1]);
+    const word = match[2];
+
+    if (word.length !== num) return false;
+
+    totalLength += match[0].length;
+  }
+
+  return totalLength === inputStr.length;
+};
