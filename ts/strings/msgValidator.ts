e13602cc12612c2f0c96c230a6c8308eb3c0e874
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-07-01 12:08:19 +0100

    add jsdoc comments for msgValidator

--- ts/strings/msgValidator.ts
+++ ts/strings/msgValidator.ts
@@ -1,3 +1,22 @@
+/**
+ *
+ * @param inputStr
+ * @returns {boolean}
+ *
+ * returns true if char count of a word in a given string matches the value of the number preceding it or if the string is empty
+ * returns false otherwise
+ *
+ * regexp matches multiple, adjacent alpha-numeric chars
+ * uses .exec method to return an array of values matched by regexp
+ * iterates through the matches until it finishes the end of the given string (i.e., when regexp.exec(inputStr) === null)
+ * pulls [1] - the numeral in the string - and coerces the type to Number using the Number() constructor
+ * pulls [2] - the word in the string
+ * returns false if word.length !== num
+ * adds the total chars processing through valid (number, word) pairs to totalLength
+ * if totalLength === inputStr.length, there are no leftover characters so we can return true
+ * else, there are leftover chars (number without word, or vice versa) - return false;
+ */
+
 const msgValidator = (inputStr: string): boolean => {
   const regexp: RegExp = /(\d+)([a-zA-Z]+)/g;
 
